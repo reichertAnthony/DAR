a3eadcba2a836c0bd20d977487036c8a060fedc5
jfitoussi <jonathan.fitoussi@me.com>
2014-10-13 17:52:36 +0200

    todo

---
+++ src/main/java/fr/xebia/gae/todo/api/v2/TodoEndPoint.java
@@ -0,0 +1,44 @@
+package fr.xebia.gae.todo.api.v2;
+
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.Named;
+import fr.xebia.gae.todo.api.v2.model.Todo;
+import fr.xebia.gae.todo.api.v2.repository.TodoRepository;
+
+import java.util.Collection;
+
+@Api(
+    name = "todos",
+    version = "v2"
+)
+public class TodoEndPoint {
+
+    @ApiMethod(name = "list", httpMethod = ApiMethod.HttpMethod.GET, path="list")
+    public Collection<Todo> getTodos() {
+        return TodoRepository.getInstance().findTodos();
+    }
+
+    @ApiMethod(name = "listbyTitle", httpMethod = ApiMethod.HttpMethod.GET, path="listbyTitle")
+    public Todo getTodosbyTitle(@Named("titre") String titre) {
+        return TodoRepository.getInstance().findTodosbyTitle(titre);
+    }
+
+    
+    @ApiMethod(name = "create", httpMethod =  ApiMethod.HttpMethod.POST, path="create")
+    public Todo create(Todo todo) {
+        return TodoRepository.getInstance().create(todo);
+    }
+
+    @ApiMethod(name = "update", httpMethod =  ApiMethod.HttpMethod.PUT, path="update")
+    public Todo update(Todo editedTodo) {
+        return TodoRepository.getInstance().update(editedTodo);
+    }
+
+    @ApiMethod(name = "remove", httpMethod = ApiMethod.HttpMethod.DELETE, path="remove")
+    public void remove(@Named("id") Long id) {
+        TodoRepository.getInstance().remove(id);
+    }
+
+}
